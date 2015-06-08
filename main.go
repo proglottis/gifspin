ed50dd0cf0a52cdf53867390e1b36a4abb86e10b
James Fargher <proglottis@gmail.com>
2015-06-09 08:38:26 +1200

    Parallelise

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"flag"
 	"image"
 	"image/color/palette"
@@ -8,6 +9,8 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime"
+	"sync"
 
 	"golang.org/x/image/draw"
 	"golang.org/x/image/math/f64"
@@ -61,37 +64,95 @@ func createGIF(name string, img *gif.GIF) error {
 	return gif.EncodeAll(f, img)
 }
 
+type frame struct {
+	i     int
+	image *image.Paletted
+}
+
+type frameHeap []frame
+
+func (h frameHeap) Len() int           { return len(h) }
+func (h frameHeap) Less(i, j int) bool { return h[i].i < h[j].i }
+func (h frameHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *frameHeap) Push(x interface{}) {
+	*h = append(*h, x.(frame))
+}
+
+func (h *frameHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func rotatePaletted(src image.Image, sb image.Rectangle, backfill image.Image, angle float64) *image.Paletted {
+	rotated := image.NewRGBA(sb)
+	draw.Copy(rotated, image.Point{}, backfill, rotated.Bounds(), draw.Src, nil)
+	rotate(rotated, src, angle, draw.Src)
+	dst := image.NewPaletted(sb, palette.WebSafe)
+	draw.FloydSteinberg.Draw(dst, sb, rotated, sb.Min)
+	return dst
+}
+
+func generateFrames(src image.Image, steps int) <-chan frame {
+	bounds := src.Bounds()
+	backfill := image.NewUniform(src.At(bounds.Min.X, bounds.Min.Y))
+	stepAngle := 2 * math.Pi / float64(steps)
+
+	c := make(chan frame)
+	var wg sync.WaitGroup
+	wg.Add(steps)
+	for i := 0; i < steps; i++ {
+		go func(i int) {
+			log.Println("Frame", i+1, "starting")
+			dst := rotatePaletted(src, bounds, backfill, stepAngle*float64(i))
+			log.Println("Frame", i+1, "done")
+			c <- frame{
+				i:     i,
+				image: dst,
+			}
+			wg.Done()
+		}(i)
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+	return c
+}
+
 func main() {
 	var steps, delay int
 	flag.IntVar(&steps, "steps", 25, "Number of steps to complete a full rotation")
 	flag.IntVar(&delay, "delay", 1, "Amount of delay between steps in 100ths of a second")
 	flag.Parse()
 
-	stepAngle := 2 * math.Pi / float64(steps)
-
-	var frames []*image.Paletted
-	var delays []int
+	log.Println("GOMAXPROCS set to", runtime.NumCPU())
+	runtime.NumCPU()
 
 	original, err := openImage(flag.Arg(0))
 	if err != nil {
 		log.Fatal("Cannot load image: ", err)
 	}
 
-	bounds := original.Bounds()
-	backfill := image.NewUniform(original.At(bounds.Min.X, bounds.Min.Y))
-	drawer := draw.FloydSteinberg
-
-	for i := 0; i < steps; i++ {
-		log.Println("Frame", i+1)
-		rotated := image.NewRGBA(bounds)
-		draw.Copy(rotated, image.Point{}, backfill, rotated.Bounds(), draw.Src, nil)
-		rotate(rotated, original, float64(i)*stepAngle, draw.Src)
-		frame := image.NewPaletted(bounds, palette.WebSafe)
-		drawer.Draw(frame, bounds, rotated, bounds.Min)
-		frames = append(frames, frame)
+	h := &frameHeap{}
+	heap.Init(h)
+	for f := range generateFrames(original, steps) {
+		heap.Push(h, f)
+		log.Println("Frame", f.i+1, "queued")
+	}
+	var frames []*image.Paletted
+	var delays []int
+	for h.Len() > 0 {
+		f := heap.Pop(h).(frame)
+		log.Println("Frame", f.i+1, "dequeued")
+		frames = append(frames, f.image)
 		delays = append(delays, delay)
 	}
 
+	log.Println("Writing to", flag.Arg(1))
 	if err := createGIF(flag.Arg(1), &gif.GIF{Image: frames, Delay: delays}); err != nil {
 		log.Fatal("Cannot create image: ", err)
 	}
